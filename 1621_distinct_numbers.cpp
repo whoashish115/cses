7a86a08290f9fe61e568e1ad2bf12445c2d7e8f3
Ashish Kumar <who.ashish115@gmail.com>
2026-06-30 23:01:51 +0530

    solve: 1621 - distinct numbers

---
+++ 1621_distinct_numbers.cpp
@@ -0,0 +1,41 @@
+/*
+========================================
+Problem  : Distinct Numbers
+URL      : https://cses.fi/problemset/task/1621
+Code     : https://github.com/whoashish115/cses
+Author   : Ashish Kumar
+Language : C++20
+========================================
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    int n;
+    cin >> n;
+    int temp;
+    set<int> s;
+    while (n--)
+    {
+        cin >> temp;
+        s.insert(temp);
+    }
+    cout << s.size() << '\n';
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
